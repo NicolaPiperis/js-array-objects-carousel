ab0b6ae3588dc191ceda77a575634233c2244a23
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-17 12:34:57 +0200

    destructuring and card creation

--- js/main.js
+++ js/main.js
@@ -25,17 +25,60 @@ const images = [
         text: 'Marvels Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
-
+// VARIABILI GLOBALI
+const container = document.getElementById("container");
+const containerImg = document.querySelectorAll(".container_img");
+let imagePosition = 0;
 
 // Creare un carosello come nella foto allegata.
-// Milestone 0:
 // Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider.
+images.forEach((element, index) => {
+
+    // DESTRUTTURAZIONE
+    let {image, title, text} = element;
+
+    // CREAZIONE ELEMENTO HTML DEL CONTENUTO PRINCIPALE
+    let card = 
+    `<div class="container_img hidden">
+
+    <div id="next" class="container_icon_up"><i class="fa-solid fa-arrow-up" style="color: #1e3050;"></i></div>
+    
+         <img src="${image}">  
+
+    <div id="prev" class="container_icon_down"><i class="fa-solid fa-arrow-down"></i></div>
+
+    <div class="text">
+        <h2>${title}</h2>
+        <p>${text}</p>
+    </div>
+
+    </div>`
+
+    // IMMETTO IL CONTENUTO PRINCIPALE NEL DOM E ATTRAVERSO IL CICLO MI OCCUPO DI CREARE TANTE IMMAGINI QUANTI OGGETTI ALL'INTERNO DELL'ARRAY
+    container.innerHTML += card;
+    }
+)
 // Milestone 1:
 // Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+for(let i = 0; i < images.length; i++) {   
+    
+    let containerImgIesimo = containerImg[i];
+    if(i === imagePosition) {
+        containerImgIesimo.classList.remove("hidden");
+        containerImgIesimo.classList.add("active");
+    } 
+}
+
 // Al click dell’utente sulle frecce verso sopra e sotto, l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
+
 // Milestone 2:
 // Aggiungere il **ciclo infinito** del carosello.
 // Ovvero se l’immagine attiva è la prima e l’utente clicca la freccia verso sopra, l’immagine che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sotto.
+
+
+
+
 // BONUS 1:
 // Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
 // BONUS 2:
