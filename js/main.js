3f28b2a418ce56bcd65dafc09b90c67b347f63ce
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-17 15:31:30 +0200

    update click topside arrow

--- js/main.js
+++ js/main.js
@@ -25,10 +25,9 @@ const images = [
         text: 'Marvels Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
 // VARIABILI GLOBALI
 const container = document.getElementById("container");
-const containerImg = document.querySelectorAll(".container_img");
-let imagePosition = 0;
 
 // Creare un carosello come nella foto allegata.
 // Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider.
@@ -58,7 +57,12 @@ images.forEach((element, index) => {
     container.innerHTML += card;
     }
 )
-// Milestone 1:
+
+// VARIABILI 
+const containerImg = document.querySelectorAll(".container_img");
+
+let imagePosition = 0;
+
 // Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
 for(let i = 0; i < images.length; i++) {   
     
@@ -68,10 +72,21 @@ for(let i = 0; i < images.length; i++) {
         containerImgIesimo.classList.add("active");
     } 
 }
-
 // Al click dell’utente sulle frecce verso sopra e sotto, l’immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+// ARROWS DICHIARAZIONI
+let topside = document.getElementById("next");
+let downside = document.getElementById("prev");
+console.log(next, prev);
 
-
+topside.addEventListener("click",
+    function(){
+        if(imagePosition < (images.length - 1))
+        containerImg[imagePosition].classList.remove("hidden");
+        
+        imagePosition++
+        containerImg[imagePosition].classList.add("active");
+    }
+)
 // Milestone 2:
 // Aggiungere il **ciclo infinito** del carosello.
 // Ovvero se l’immagine attiva è la prima e l’utente clicca la freccia verso sopra, l’immagine che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sotto.
